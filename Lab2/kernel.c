c2bf5695985ecb789e8bd318b288f1ad42566e48
Alex Karwowski <51871385+AlikanakelaKarwowski@users.noreply.github.com>
2020-02-14 12:31:24 -0500

    comitt formating stuff

--- Lab2/kernel.c
+++ Lab2/kernel.c
@@ -42,18 +42,18 @@ void main()
    makeInterrupt21();
    printLogo();
    interrupt(33,0,"\r\nWelcome to the Mad Libs kernel.\r\n\0",0,0);
-   interrupt(33,0,"Enter a food: \r\n\0",0,0); /* DIDNT READ */
+   interrupt(33,0,"\r\nEnter a food: \0",0,0); /* DIDNT READ */
    interrupt(33,1,food,0,0);
    temp = 0;
-   interrupt(33,0,"Enter a number between 100 and 120: \r\n\0",0,0); /* DIDNT READ */
+   interrupt(33,0,"\r\nEnter a number between 100 and 120: \0",0,0); /* DIDNT READ */
    interrupt(33,14,&temp,0,0);
-   interrupt(33,0,"Enter an adjective: \r\n\0",0,0); /* DIDNT READ */
+   interrupt(33,0,"\r\nEnter an adjective: \0",0,0); /* DIDNT READ */
    interrupt(33,1,adjective,0,0);
-   interrupt(33,0,"Enter a color: \r\n\0",0,0);
+   interrupt(33,0,"\r\nEnter a color: \0",0,0);
    interrupt(33,1,color,0,0);
-   interrupt(33,0,"Enter an animal: \r\n\0",0,0);
+   interrupt(33,0,"\r\nEnter an animal: \0",0,0);
    interrupt(33,1,animal,0,0);
-   interrupt(33,0,"Your note is on the printer, go get it.\r\n\0",0,0);
+   interrupt(33,0,"\r\nYour note is on the printer, go get it.\r\n\0",0,0);
    interrupt(33,0,"Dear Professor O\'Neil,\r\n\0",1,0);
    interrupt(33,0,"\r\nI am so sorry that I am unable to turn in my program at this time.\r\n\0",1,0);
    interrupt(33,0,"First, I ate a rotten \0",1,0);
@@ -163,7 +163,6 @@ void readInt(int* n)
 {
     int i;
     char c[80];
-    n=0;
     readString(c);
 
     /* wont compile when i is declared inside for loop */

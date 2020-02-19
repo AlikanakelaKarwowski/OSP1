0b76d10e46fbefa565a8f8fb85312955b6113ac1
Alex Karwowski <51871385+AlikanakelaKarwowski@users.noreply.github.com>
2020-02-19 09:25:52 -0500

    Add carriage and newline return

--- Lab3/kernel.c
+++ Lab3/kernel.c
@@ -71,6 +71,9 @@ void printString(char* c, int d)
             interrupt(16, ax, 0, 0, 0);
             c++;
         }
+    /*add new line and return carrage*/
+    interrupt(16,'\r',0,0,0);
+    interrupt(16,'\n',0,0,0);
     /* fill this in */
     return;
 }

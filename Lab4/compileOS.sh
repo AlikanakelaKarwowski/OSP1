3c44068ed8a7fce83b9041ae17199a8bd32d82d9
Alex K <alexkarrrr@gmail.com>
2020-02-24 15:36:29 -0500

    Update compileos

--- Lab4/compileOS.sh
+++ Lab4/compileOS.sh
@@ -5,3 +5,4 @@ as86 kernel.asm -o kasm.o
 ld86 -o kernel -d kernel.o kasm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
 dd if=msg of=floppya.img bs=512 count=1 seek=30 conv=notrunc
+dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc

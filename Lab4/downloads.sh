5a885360f1c077e55ad3e94dfacc245fafc7fced
Alex Karwowski <51871385+AlikanakelaKarwowski@users.noreply.github.com>
2020-02-25 14:43:10 -0500

    Add downloads script for linux testing

---
+++ Lab4/downloads.sh
@@ -0,0 +1,6 @@
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.asm
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.h
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/fib.c
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty1
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty2
+wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/Stenv

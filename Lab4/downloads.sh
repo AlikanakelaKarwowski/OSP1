2c4975ae494ad8fecf7e3e5eedb402e973da91f9
Alex Karwowski <51871385+AlikanakelaKarwowski@users.noreply.github.com>
2020-02-25 15:00:21 -0500

    fix download script

--- Lab4/downloads.sh
+++ Lab4/downloads.sh
@@ -1,6 +1,6 @@
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.asm
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.h
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/fib.c
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty1
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty2
-wget http://www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/Stenv
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.asm && echo "Downloaded blackdos.asm"
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/blackdos.h && echo "Downloaded blackdos.h"
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/fib.c && echo "Downloaded fib.c"
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty1 && echo "Downloaded kitty1"
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/kitty2 && echo "Downloaded kitty2"
+wget -q www.cs.uakron.edu/~toneil/teaching/cs426/blackdos/lab04%20load%20exec%20progs/Stenv && echo "Downloaded Stenv"

53bac6de37313f3fcf4cfe74f85a65bc71b3d59e
Alex K <alexkarrrr@gmail.com>
2020-03-31 17:31:11 -0400

    idk

--- Lab5/Shell.c
+++ Lab5/Shell.c
@@ -6,57 +6,7 @@
 #define SCANN(x)   interrupt(33,14,&x,0,0)
 #define END        interrupt(33,5,0,0,0)
 
-int CommandHandler(char *c ){
-    /* boot */
-    if (c[0] == 'b' && c[1] == 'o' && c[2] == 'o' && c[3] == 't')
-        {return 1;}
-
-    /* clrs */
-    else if (c[0] == 'c' && c[1] == 'l' && c[2] == 'r' && c[3] == 's')
-        {return 2;}
-
-    /* echo */
-    else if (c[0] == 'e' && c[1] == 'c' && c[2] == 'h' && c[3] == 'o')
-        {return 3;}
-
-    /* copy */
-    else if (c[0] == 'c' && c[1] == 'o' && c[2] == 'p' && c[3] == 'y')
-        {return 4;}
-
-    /* ddir */
-    else if (c[0] == 'd' && c[1] == 'd' && c[2] == 'i' && c[3] == 'r')
-        {return 5;}
-
-    /* exec */
-    else if (c[0] == 'e' && c[1] == 'x' && c[2] == 'e' && c[3] == 'c')
-        {return 6;}
-
-    /* help */
-    else if (c[0] == 'h' && c[1] == 'e' && c[2] == 'l' && c[3] == 'p')
-        {return 7;}
-
-    /* prnt */
-    else if (c[0] == 'p' && c[1] == 'r' && c[2] == 'n' && c[3] == 't')
-        {return 8;}
-
-    /* remv */
-    else if (c[0] == 'r' && c[1] == 'e' && c[2] == 'm' && c[3] == 'v')
-        {return 9;}
-
-    /* senv */
-    else if (c[0] == 's' && c[1] == 'e' && c[2] == 'n' && c[3] == 'v')
-        {return 10;}
-
-    /* show */
-    else if (c[0] == 's' && c[1] == 'h' && c[2] == 'o' && c[3] == 'w')
-        {return 11;}
-
-    /* twet */
-    else if (c[0] == 't' && c[1] == 'w' && c[2] == 'e' && c[3] == 't')
-        {return 12;}
-
-}
-
+int CommandHandler(char *c );
 
 void main()
 {
@@ -126,4 +76,55 @@ void main()
         }
 
     }
+}
+
+int CommandHandler(char *c ){
+    /* boot */
+    if (c[0] == 'b' && c[1] == 'o' && c[2] == 'o' && c[3] == 't')
+        {return 1;}
+
+    /* clrs */
+    else if (c[0] == 'c' && c[1] == 'l' && c[2] == 'r' && c[3] == 's')
+        {return 2;}
+
+    /* echo */
+    else if (c[0] == 'e' && c[1] == 'c' && c[2] == 'h' && c[3] == 'o')
+        {return 3;}
+
+    /* copy */
+    else if (c[0] == 'c' && c[1] == 'o' && c[2] == 'p' && c[3] == 'y')
+        {return 4;}
+
+    /* ddir */
+    else if (c[0] == 'd' && c[1] == 'd' && c[2] == 'i' && c[3] == 'r')
+        {return 5;}
+
+    /* exec */
+    else if (c[0] == 'e' && c[1] == 'x' && c[2] == 'e' && c[3] == 'c')
+        {return 6;}
+
+    /* help */
+    else if (c[0] == 'h' && c[1] == 'e' && c[2] == 'l' && c[3] == 'p')
+        {return 7;}
+
+    /* prnt */
+    else if (c[0] == 'p' && c[1] == 'r' && c[2] == 'n' && c[3] == 't')
+        {return 8;}
+
+    /* remv */
+    else if (c[0] == 'r' && c[1] == 'e' && c[2] == 'm' && c[3] == 'v')
+        {return 9;}
+
+    /* senv */
+    else if (c[0] == 's' && c[1] == 'e' && c[2] == 'n' && c[3] == 'v')
+        {return 10;}
+
+    /* show */
+    else if (c[0] == 's' && c[1] == 'h' && c[2] == 'o' && c[3] == 'w')
+        {return 11;}
+
+    /* twet */
+    else if (c[0] == 't' && c[1] == 'w' && c[2] == 'e' && c[3] == 't')
+        {return 12;}
+
 }
